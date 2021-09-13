d4095063535cb9a3d50261c34c3a71a32f859265
MohamadNajjar <moe.najjar.01@gmail.com>
2021-09-13 12:35:04 +0300

    changing

--- game.js
+++ game.js
@@ -20,12 +20,25 @@ var config = {
 var game = new Phaser.Game(config);
     function preload(){
         this.load.image('background','assets/background.png');
+        this.load.image('message-initial','assets/message-initial.png');
         this.load.image('bird','assets/bird.png');
-        this.load.image('pipe','assets/pipe.png');
+        this.load.image('pipe-green-top','assets/pipe-green-top.png');
+        this.load.image('pipe-green-bottom','assets/pipe-green-bottom.png');
+        this.load.image('number1','assets/number1.png');
+        this.load.image('number2','assets/number2.png');
+        this.load.image('number3','assets/number3.png');
+        this.load.image('number4','assets/number4.png');
+        this.load.image('number5','assets/number5.png');
+        this.load.image('number6','assets/number6.png');
+        this.load.image('number7','assets/number7.png');
+        this.load.image('number8','assets/number8.png');
+        this.load.image('number9','assets/number9.png');
+        this.load.image('gameover','assets/gameover.png');
+        this.load.image('restart-button','assets/restart-button.png');
     }
 
     function create(){
-
+        this.add.image(144,256,'background');
     }
 
     function update(){
